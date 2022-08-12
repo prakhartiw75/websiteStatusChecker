a35993c813e02584f63712da9c45f95cc9ab93e6
Prakhar Tiwari <prakhar.tiwari1@MacBook-Pro.local>
2022-08-12 17:56:05 +0530

    Added get and post api

--- build/service/service.go
+++ build/service/service.go
@@ -19,13 +19,23 @@ func HelloHandler(writer http.ResponseWriter, req *http.Request) {
 		for _, val := range website {
 			_, err := http.Get("http://" + val)
 			if err != nil {
-				status[val] = "Down"
+				status[val] = "DOWN"
 			} else {
 				status[val] = "UP"
 			}
 		}
 		fmt.Println("After Post Logic is executed=", status)
 	case "GET":
+		value := req.URL.Query().Get("website")
+		if value != "" {
+
+			jsonVal, err := json.Marshal(status[value])
+			if err != nil {
+				fmt.Println("Error occured=", err.Error())
+			}
+			writer.Write(jsonVal)
+			return
+		}
 		fmt.Println("Before Get Logic is executed=", status)
 		jsonVal, err := json.Marshal(status)
 		if err != nil {
